5a792470c23af540cf5e3d7ab84855b2869ed8a1
ra7mouni <justamin02@gmail.com>
2023-05-08 12:01:30 +0100

    Factorial

--- Week2/Day 1/Practice/Loop Challenges/script.js
+++ Week2/Day 1/Practice/Loop Challenges/script.js
@@ -18,3 +18,10 @@ for( var i = 0 ; i < 101 ; i++){
     sum += i ;
 }
 console.log(sum);
+
+//Factorial//
+var product = 1 ;
+for(var i = 1 ; i < 13 ; i++){
+    product*=i;
+}
+console.log(product)
