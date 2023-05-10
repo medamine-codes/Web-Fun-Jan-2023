f92d311393c9be7c0815e0189af4f18459ce184d
ra7mouni <justamin02@gmail.com>
2023-05-10 21:00:19 +0100

    liking likes

---
+++ Week2/Day 1/Core/Liking Likes/script.js
@@ -0,0 +1,15 @@
+var count = 0;
+function counterFunc(){
+    count++;
+    document.querySelector(".num").innerHTML = count;
+}
+var count1 = 0;
+function counterFunc1(){
+    count++;
+    document.querySelector(".num1").innerHTML = count;
+}
+var count2 = 0;
+function counterFunc2(){
+    count++;
+    document.querySelector(".num2").innerHTML = count;
+}
